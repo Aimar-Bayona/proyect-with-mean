23a4fb0df9181b442a44150e991f5c21687b55d3
Aimar <aimaraimarbbbb@gmail.com>
2023-01-26 22:37:18 -0500

    validations and permissions were expanded, in addition to the model

--- validators/app.validator.js
+++ validators/app.validator.js
@@ -1,17 +1,39 @@
-const { check } = require('express-validator')
-const { validateResult } = require('../helpers/app.helper')
+const { check } = require("express-validator");
+const { validateResult } = require("../helpers/app.helper");
 const validateCreate = [
-    check('name')
+  check("name").isLength({ min: 3 }).isAlpha(),
+  check("secondName").default("null").isAlpha(),
+  check("lastName").isLength({ min: 4 }).isAlpha(),
+  check("secondLastName").default("null").isAlpha(),
+  check("user").exists(),
+  check("password").isStrongPassword(),
+  check("email").isEmail(),
+  check("phone")
+    .isMobilePhone()
     .exists()
-    .isLength({min:3})
-    .not()
-    .isEmpty()
-    .not()
-    .isNumeric(),
-    (req, res, next) =>{
-        validateResult(req, res, next)
-    }
-]
+    .custom((numberPhone) => {
+      if (!validateNumberPhone(numberPhone)) {
+        throw new Error("wrong size");
+      }
+      return true;
+    }),
+  check("dateBorn").exists().toDate(),
+  (req, res, next) => {
+    validateResult(req, res, next);
+  },
+];
 
+function validateNumberPhone(numberPhone) {
+  if (size(numberPhone) == 10 || size(numberPhone) == 7) return true;
+}
 
-module.exports = { validateCreate }
+function size(numberPhone) {
+  let acum = 0;
+  while (numberPhone != 0) {
+    numberPhone = parseInt(numberPhone / 10);
+    acum++;
+  }
+  return acum;
+}
+
+module.exports = { validateCreate };
